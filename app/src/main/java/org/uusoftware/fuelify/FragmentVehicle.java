49fc84060be76d906473bb438ecb6830e92f2800
Aykut Uludağ <aykutuludag1@gmail.com>
2018-05-15 15:29:32 +0300

    sadeleştirmeler

--- app/src/main/java/org/uusoftware/fuelify/FragmentVehicle.java
+++ app/src/main/java/org/uusoftware/fuelify/FragmentVehicle.java
@@ -24,10 +24,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-<<<<<<< HEAD
 import com.bumptech.glide.Glide;
-=======
->>>>>>> origin/master
 import com.github.curioustechizen.ago.RelativeTimeTextView;
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
